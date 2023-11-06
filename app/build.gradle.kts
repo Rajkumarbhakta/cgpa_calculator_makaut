3ea683607250474ccbe088f3993cc55c5a9194b0
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2023-11-07 00:56:53 +0530

    version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rkbapps.makautsgpaygpacalculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
