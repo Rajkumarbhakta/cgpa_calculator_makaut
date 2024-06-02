78b67eb8b2ab2aada83dad61e6c8d823ec2ff39a
Rajkumarbhakta <rajkumar.bhakta@redoq.co.uk>
2024-06-02 23:33:29 +0530

    upgraded to 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rkbapps.makautsgpaygpacalculator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "2.3"
+        versionCode = 6
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
