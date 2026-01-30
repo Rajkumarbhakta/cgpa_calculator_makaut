d3c125ce5d0c38b5cb28f42ef303b040364da860
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2026-01-30 21:39:33 +0530

    update: increment version code to 13 and version name to 3.1 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.rkbapps.makautsgpaygpacalculator"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "3.0"
+        versionCode = 13
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
